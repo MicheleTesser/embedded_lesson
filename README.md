eb18336f2817342514791be454e3e6d1a621691f
Alberto Damo <alberto.damo@gmail.com>
2024-11-05 22:28:57 +0100

    Enjoy and have fun ;)

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
+# Embedded lesson
+
+## How to start
 To work on the project fork the repository in your own Raceup GitHub profile.
+
+## Requirements
+#[no_std] must remain, you are not allowed to use any external library.
+
+## What to do
+- What does it mean to use #[no_std] in a rust project, why may it be necessary?
+- What does it mean to extern crate alloc; in a rust project and why may it be necessary?
+
+- Define a trait that represents a Mex with the following functions where data and id are 
+  generic parameters:
+  - new( id, length, data): return a new Mex
+  - id(): given a Mex return its id
+  - dlc(): given a Mex return its length
+  - data(): given a Mex return its data
+
+- Define a trait that represent the usage of a generic bus where
+  - the trait has a generic parameter <P> with the constraint that <P> has to implement 
+    the Mex trait
+  - the following functions must exist:
+        - new: initializes a new bus returning the object that implements the trait
+        - send: use the object that implement the trait, take a <P> type and send it in the bus.
+            return a Result with type () if Ok and Err(&str) if some errors occur
+        - receive: use the object that implement the trait,
+            return a <P> type populated with the data obtained from the bus
+
+- Implement the following protocols with the trait above:
+  - CAN base (id 11-bit)
+  - Serial
+ For each implementation define some unit-testing where you define at least 
+ one function for each function defined in the implementation
+
+ Don't worry if you don't complete.
+
+## Troubleshooting
+ If you have an amd computer delete the .cargo directory or change the target accordingly
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,11 @@ extern crate alloc;
 /*
  * #[no_std] must remain and you are not allowed to use any external library.
  *
- * 1- what does it mean to use #[no_std] in a rust project and why it may be necessary?
- * 2- what does it mean to extern crate alloc; in a rust project and why it may be necessary?
+ * 1- what does it mean to use #[no_std] in a rust project and why may it be necessary?
+ * 2- what does it mean to extern crate alloc; in a rust project and why may it be necessary?
  *
- * 3- define a trait that represent a Mex with the following functions where data and id are 
- *    generics  parameters:
+ * 3- define a trait that represents a Mex with the following functions where data and id are 
+ *    generic  parameters:
  *    - new(id,length,data): return a new Mex
  *    - id(): given a Mex return its id
  *    - dlc(): given a Mex return its length
@@ -18,22 +18,20 @@ extern crate alloc;
  *    - the trait has a generic parameter <P> with the constraint that <P> has to implement 
  *      the Mex trait
  *    - the following functions must exist:
- *          - new: initialized a new bus returning the object that implement the trait
+ *          - new: initializes a new bus returning the object that implements the trait
  *          - send: use the object that implement the trait, take a <P> type and send it in the bus.
- *              return a Result with type () if Ok and Err(&str) if some errors occurs
+ *              return a Result with type () if Ok and Err(&str) if some errors occur
  *          - receive: use the object that implement the trait,
  *              return a <P> type populated with the data obtained from the bus
  *
  * 5- Implement the following protocols with the trait above:
  *    - CAN base (id 11-bit)
  *    - Serial
- *  For each of the implementation define some unit-testing where you define at least 
+ *  For each implementation define some unit-testing where you define at least 
  *  one function for each function defined in the implementation
  *
  *  IF YOU HAVE AN AMD COMPUTER DELETE THE .cargo DIRECTORY OR CHANGE THE target accordingly
  *
  *
- *  If you don't complete the request on saturday it's not a problem. The project itself does not
- *  have a deadline. It is for you to learn the language and what challenges you will face later
- *  on.
+ *  Don't worry if you don't complete.
  */
